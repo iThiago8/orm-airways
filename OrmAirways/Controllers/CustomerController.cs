8916ad1e643432205c08e1fbb140194e39d0595d
Thiago Pedro Padilha <thiago.pedro.padilha899@gmail.com>
2025-10-05 23:13:35 -0300

    feat: Customer Create view and delete

--- OrmAirways/Controllers/CustomerController.cs
+++ OrmAirways/Controllers/CustomerController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OrmAirways.Interfaces;
+using OrmAirways.ViewModels.Customer;
+using OrmAirways.Models;
+using System.Net.WebSockets;
 
 namespace OrmAirways.Controllers
 {
@@ -8,13 +11,59 @@ namespace OrmAirways.Controllers
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            return View(await customerRepository.GetAll());
+            var customers = await customerRepository.GetAll();
+
+            if (customers == null)
+                return View();
+
+            ICollection<CustomerViewModel> viewModels = [];
+
+            foreach (var c in customers)
+            {
+                viewModels.Add(new CustomerViewModel
+                {
+                    ID = c.ID,
+                    CPF = c.CPF,
+                    Name = c.Name,
+                    PhoneNumber = c.PhoneNumber,
+                    IsVIP = c.IsVIP
+                });
+            }
+
+            return View(viewModels);
         }
 
         [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateCustomerViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(viewModel);
+
+            await customerRepository.Create(new Customer
+            {
+                CPF = viewModel.CPF,
+                Name = viewModel.Name,
+                PhoneNumber = viewModel.PhoneNumber
+            });
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var customer = await customerRepository.GetById(id);
+            if (customer == null)
+                return NotFound();
+
+            await customerRepository.Delete(customer);
+            return RedirectToAction("Index");
+
         }
     }
 }
