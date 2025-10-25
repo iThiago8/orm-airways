0f6e6b2ef29deab9e6031cdf5d6f9883ff431c40
Marcos Henrique Vermolhen de Souza Santos <429035@unoesc.lan>
2025-10-24 21:53:46 -0300

    RealizarOperações

--- OrmAirways/Controllers/AircraftController.cs
+++ OrmAirways/Controllers/AircraftController.cs
@@ -8,10 +8,57 @@ namespace OrmAirways.Controllers
 	{
 		private readonly IAircraftRepository _aircraftRepository;
 
-		
-		public IActionResult Index()
+		public AircraftController(IAircraftRepository aircraftRepository)
 		{
-			return View();
+			_aircraftRepository = aircraftRepository; //Constructor Injection; Construtor feito
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			return View(await _aircraftRepository.GetAll());
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Create(Aircraft aircraft)
+		{
+			if (ModelState.IsValid)
+			{
+				await _aircraftRepository.Create(aircraft);
+				return RedirectToAction("Index");
+			}
+
+			return View(aircraft);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Update(int? id, Aircraft aircraft)
+		{
+			if (!id.HasValue)
+			{
+				return BadRequest();
+			}
+			if (ModelState.IsValid)
+			{
+				await _aircraftRepository.Update(aircraft);
+				return RedirectToAction("Index");
+			}
+			return View(aircraft);
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Update(int? id)
+		{
+			var aircraft = await _aircraftRepository.GetById(id.Value);
+			if (aircraft == null) // = símbolo de atribuição -> == símbolo de comparação.
+			{
+				return NotFound();
+			}
+			if (!id.HasValue)
+			{
+				return BadRequest();
+			}
+			return View(aircraft);
 		}
 	}
 }
