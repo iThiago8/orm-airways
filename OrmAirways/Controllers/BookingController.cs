2b356accfb2402851826644ebb19484836f5a534
Marcos Henrique Vermolhen de Souza Santos <429035@unoesc.lan>
2025-10-31 19:57:18 -0300

    ExcluidoViewDeSeaT

--- OrmAirways/Controllers/BookingController.cs
+++ OrmAirways/Controllers/BookingController.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc;
+using OrmAirways.Interfaces;
+
+namespace OrmAirways.Controllers
+{
+	public class BookingController(IBookingRepository bookingRepository) : Controller
+	{
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			var bookings = await bookingRepository.GetAll();
+
+			if (bookings == null)
+				return View();
+
+			return View(bookings);
+		}
+	}
+}
