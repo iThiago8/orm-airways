6c9c6d17dfe92c9b25a27c8167d208e127ee1a77
411730 <411730@VDA-LCOII-19168>
2025-11-14 21:56:12 -0300

    Starting booking views

--- OrmAirways/Controllers/BookingController.cs
+++ OrmAirways/Controllers/BookingController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using OrmAirways.Interfaces;
+using OrmAirways.Models;
 
 namespace OrmAirways.Controllers
 {
@@ -14,6 +15,53 @@ namespace OrmAirways.Controllers
                 return View();
 
             return View(bookings);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Booking booking)
+        {
+            if (!ModelState.IsValid)
+                return View(booking);
+
+            await bookingRepository.Create(booking);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(int id)
+        {
+            var booking = await bookingRepository.GetById(id);
+            if (booking == null)
+                return NotFound();
+
+            return View(booking);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Booking booking)
+        {
+            if (!ModelState.IsValid)
+                return View(booking);
+
+            await bookingRepository.Update(booking);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var booking = await bookingRepository.GetById(id);
+            if (booking == null)
+                return NotFound();
+
+            await bookingRepository.Delete(booking);
+            return RedirectToAction("Index");
         }
     }
 }
